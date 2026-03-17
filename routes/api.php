513acdeb55833dae831b5abc8f41dff3b021f4d3
devprince <chuksechejunior@gmail.com>
2026-03-17 10:21:33 +0100

    Project Setup

---
+++ routes/api.php
@@ -0,0 +1,14 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::get('/status', function () {
+    return response()->json([
+        "status" => "Blog API is online"
+    ]);
+});
